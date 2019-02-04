78064f0273fa08706bff61cb73732664030a11a8
Toshiyuki HINA <thina@yourinventit.com>
2019-02-04 13:30:43 +0900

    initial commit

---
+++ lib/jquery/activity/indicator/rails.rb
@@ -0,0 +1,12 @@
+require "jquery/activity/indicator/rails/version"
+
+module Jquery
+  module Activity
+    module Indicator
+      module Rails
+        class Error < StandardError; end
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ lib/jquery/activity/indicator/rails/version.rb
@@ -0,0 +1,9 @@
+module Jquery
+  module Activity
+    module Indicator
+      module Rails
+        VERSION = "0.1.0"
+      end
+    end
+  end
+end
